f4a0286f3bf55a685d3d54bc0c6fe40cec12af3c
Daniel Schreurs <DanielSchreurs@users.noreply.github.com>
2025-12-11 20:22:59 +0100

    refactor: rename main and index files, update SCSS imports, and create package.json

--- webpack.mix.js
+++
@@ -1,7 +0,0 @@
-// webpack.mix.js
-
-let mix = require('laravel-mix');
-
-mix//.js('src/js/main.js', 'js').sourceMaps()
-    .sass('src/sass/main.scss', 'css').sourceMaps()
-    .setPublicPath('dist').browserSync("http://localhost:8000");
